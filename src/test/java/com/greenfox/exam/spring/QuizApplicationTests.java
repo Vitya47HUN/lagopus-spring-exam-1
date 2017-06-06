28042d14a5665a5f047c7d9d3bc7e3728b7edebf
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-06-06 11:45:00 +0200

    Made a working test for questions endpoint

--- src/test/java/com/greenfox/exam/spring/QuizApplicationTests.java
+++ src/test/java/com/greenfox/exam/spring/QuizApplicationTests.java
@@ -1,16 +1,44 @@
 package com.greenfox.exam.spring;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest (classes = QuizApplication.class)
+@WebAppConfiguration
+@EnableWebMvc
 public class QuizApplicationTests {
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+					MediaType.APPLICATION_JSON.getSubtype(),
+					Charset.forName("utf8"));
+	private MockMvc mockMvc;
 
-	@Test
-	public void contextLoads() {
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = webAppContextSetup(webApplicationContext).build();
 	}
 
+	@Test
+	public void testGetQuestions() throws Exception {
+		mockMvc.perform(get("/questions"))
+						.andExpect(status().isOk())
+						.andExpect(content().contentType(contentType));
+	}
 }
