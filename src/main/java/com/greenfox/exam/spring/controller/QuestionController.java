4a8926e410bb8cd538947aca2d0a1ce3251a58e7
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-06-06 09:56:55 +0200

    Added questionlist and improvements

--- src/main/java/com/greenfox/exam/spring/controller/QuestionController.java
+++ src/main/java/com/greenfox/exam/spring/controller/QuestionController.java
@@ -1,6 +1,8 @@
 package com.greenfox.exam.spring.controller;
 
 import com.greenfox.exam.spring.model.Question;
+import com.greenfox.exam.spring.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QuestionController {
 
-  @RequestMapping(path = "/questions", method = RequestMethod.POST)
+  @Autowired
+  QuestionRepository questionRepo;
+
+  @RequestMapping(path = "/questions", method = RequestMethod.GET)
   public Object getQuestions(@RequestBody Question question) {
+    questionRepo.findAll();
     return question;
   }
 }
--- src/main/java/com/greenfox/exam/spring/model/QuestionList.java
+++ src/main/java/com/greenfox/exam/spring/model/QuestionList.java
@@ -1,7 +1,41 @@
 package com.greenfox.exam.spring.model;
 
-/**
- * Created by pc on 2017.06.06..
- */
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@Component
 public class QuestionList {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  private List<Question> question;
+
+  public QuestionList(List<Question> question) {
+    this.question = question;
+  }
+
+  public QuestionList(){
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public List<Question> getQuestion() {
+    return question;
+  }
+
+  public void setQuestion(List<Question> question) {
+    this.question = question;
+  }
 }
